36ee951651505a10be3d6508a5fbb1b276d437a6
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-07 09:55:13 +0200

    Remove runtime bridge causing admin loading loop

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,6 @@
 import type { ReactNode } from "react";
 import "./phase2.css";
-import { AdminRuntimeBridge } from "./AdminRuntimeBridge";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <AdminRuntimeBridge />
-      {children}
-    </>
-  );
+  return children;
 }
