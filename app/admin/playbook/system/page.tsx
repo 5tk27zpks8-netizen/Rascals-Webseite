a81830a78eba23a948659a29a7c54c078f65389e
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-11 21:55:42 +0200

    Remove playbook system editor route from website app

--- app/admin/playbook/system/page.tsx
+++
@@ -1,5 +0,0 @@
-import { requireChatGPTUser } from "../../../chatgpt-auth";
-import { PlaybookManager } from "../PlaybookManager";
-
-export const metadata={title:"Playbook System Editor · Rascals OS",robots:{index:false,follow:false}};
-export default async function PlaybookSystemPage(){await requireChatGPTUser("/admin/playbook/system");return <PlaybookManager/>}
