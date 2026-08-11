2ba9cbf937f234109de1182de009f54c60b28a62
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-11 23:30:36 +0200

    Enable CMS section media on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,6 @@
 import { CmsHero } from "./CmsHero";
 import { DynamicHomeFeeds } from "./DynamicHomeFeeds";
 import { DynamicHomeGames } from "./DynamicHomeGames";
+import { HomeSectionMedia } from "./HomeSectionMedia";
 import { SiteShell } from "./SiteShell";
-
-export default function Home() {
-  return (
-    <>
-      <CmsHero />
-      <SiteShell page="home" />
-      <DynamicHomeFeeds />
-      <DynamicHomeGames />
-    </>
-  );
-}
+export default function Home(){return <><CmsHero/><SiteShell page="home"/><DynamicHomeFeeds/><DynamicHomeGames/><HomeSectionMedia/></>}
