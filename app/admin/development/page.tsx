686e3eefede944a420de3eb425621f3bbe1e47bd
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-11 21:54:18 +0200

    Remove development route from website app

--- app/admin/development/page.tsx
+++
@@ -1,9 +0,0 @@
-import { requireChatGPTUser } from "../../chatgpt-auth";
-import { DevelopmentManager } from "./DevelopmentManager";
-
-export const metadata = { title: "Player Development · Rascals CMS", robots: { index: false, follow: false } };
-
-export default async function DevelopmentPage(){
-  await requireChatGPTUser("/admin/development");
-  return <DevelopmentManager/>;
-}
