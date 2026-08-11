1c431ad0b2f462bb138e7d93193e9ac6317fb6e0
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-11 21:54:25 +0200

    Remove team development route from website app

--- app/admin/development/team/page.tsx
+++
@@ -1,5 +0,0 @@
-import { requireChatGPTUser } from "../../../chatgpt-auth";
-import { TeamDevelopmentManager } from "./TeamDevelopmentManager";
-
-export const metadata={title:"Team Development Command Center · Rascals OS",robots:{index:false,follow:false}};
-export default async function TeamDevelopmentPage(){await requireChatGPTUser("/admin/development/team");return <TeamDevelopmentManager/>}
