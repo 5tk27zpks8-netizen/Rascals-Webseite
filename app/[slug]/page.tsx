f9686c639e2741e9cae7cb332e86db7c119e0585
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-12 13:00:02 +0200

    Mount football history timeline on About page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AboutTimeline } from "../AboutTimeline";
 import { SiteShell, type PageName } from "../SiteShell";
 
 const pages: PageName[] = ["ueber-uns", "team", "sponsoring", "shop", "news", "galerie"];
@@ -5,5 +6,5 @@ const pages: PageName[] = ["ueber-uns", "team", "sponsoring", "shop", "news", "g
 export default async function ContentPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   const page = pages.includes(slug as PageName) ? (slug as PageName) : "home";
-  return <SiteShell page={page} />;
+  return <><SiteShell page={page} />{page === "ueber-uns" && <AboutTimeline />}</>;
 }
