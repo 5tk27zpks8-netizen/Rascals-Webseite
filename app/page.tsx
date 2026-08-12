f80b142ad7aa96c5404ce88fef10c09b87578af1
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-12 14:20:26 +0200

    Render website builder homepage server-side when enabled

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,17 @@ import { CmsHero } from "./CmsHero";
 import { DynamicHomeFeeds } from "./DynamicHomeFeeds";
 import { DynamicHomeGames } from "./DynamicHomeGames";
 import { HomeSectionMedia } from "./HomeSectionMedia";
-import { SiteBuilderGate } from "./SiteBuilderGate";
+import { SiteBuilderPage } from "./SiteBuilderPage";
 import { SiteShell } from "./SiteShell";
+import { findBuilderPage, readSiteBuilderState } from "./lib/site-builder";
 
-export default function Home(){
-  return <SiteBuilderGate slug="" fallback={<><CmsHero/><SiteShell page="home"/><DynamicHomeFeeds/><DynamicHomeGames/><HomeSectionMedia/></>}/>;
+export default async function Home() {
+  const state = await readSiteBuilderState();
+  const builderPage = findBuilderPage(state, "");
+
+  if (builderPage?.enabled) {
+    return <SiteBuilderPage state={state} page={builderPage} />;
+  }
+
+  return <><CmsHero/><SiteShell page="home"/><DynamicHomeFeeds/><DynamicHomeGames/><HomeSectionMedia/></>;
 }
