716d4ad5c6c09bf61907bc4fc1d9f263ae9f3cbc
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-12 14:00:38 +0200

    Enable website builder on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,9 @@ import { CmsHero } from "./CmsHero";
 import { DynamicHomeFeeds } from "./DynamicHomeFeeds";
 import { DynamicHomeGames } from "./DynamicHomeGames";
 import { HomeSectionMedia } from "./HomeSectionMedia";
+import { SiteBuilderGate } from "./SiteBuilderGate";
 import { SiteShell } from "./SiteShell";
-export default function Home(){return <><CmsHero/><SiteShell page="home"/><DynamicHomeFeeds/><DynamicHomeGames/><HomeSectionMedia/></>}
+
+export default function Home(){
+  return <SiteBuilderGate slug="" fallback={<><CmsHero/><SiteShell page="home"/><DynamicHomeFeeds/><DynamicHomeGames/><HomeSectionMedia/></>}/>;
+}
