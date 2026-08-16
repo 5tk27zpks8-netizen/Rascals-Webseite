1d5de31c5cd4b8d453dd74eddd97029ac0af90cf
5tk27zpks8-netizen <5tk27zpks8@privaterelay.appleid.com>
2026-08-16 19:40:02 +0200

    Move typography editing into contextual website editor

--- app/admin/website/page.tsx
+++ app/admin/website/page.tsx
@@ -1,7 +1,6 @@
 import { requireChatGPTUser } from "../../chatgpt-auth";
 import { InlineImagePositionAddon } from "./InlineImagePositionAddon";
 import { LiveWebsiteMirror } from "./LiveWebsiteMirror";
-import { TypographyStudioAddon } from "./TypographyStudioAddon";
 import { WebsiteBuilder } from "./WebsiteBuilder";
 import "./website-builder-v3.css";
 
@@ -12,5 +11,5 @@ export const metadata = {
 
 export default async function AdminWebsitePage() {
   await requireChatGPTUser("/admin/website");
-  return <><TypographyStudioAddon/><WebsiteBuilder/><LiveWebsiteMirror/><InlineImagePositionAddon/></>;
+  return <><WebsiteBuilder/><LiveWebsiteMirror/><InlineImagePositionAddon/></>;
 }
